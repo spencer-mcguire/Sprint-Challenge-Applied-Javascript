8f34e77ab392e8410f549743ea9d81e67fe7724d
spencer-mcguire <smcguire1826@gmail.com>
2019-11-01 10:29:38 -0500

    update readme create header create tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+let URL = 'https://lambda-times-backend.herokuapp.com/topics';
+let tabArray = [];
+
+axios.get(URL)
+    .then(res => {
+        tabArray = res.data.topics
+    })
+    .catch(err => {
+        console.log('DATA NOT RETURNED:', err)
+    })
+
+    setTimeout(() => {
+        console.log(tabArray)
+        tabArray.forEach(item => {
+            const newTab = Tabs(item);
+            let entry = document.querySelector('.topics')
+            entry.appendChild(newTab)
+        })
+    },500)
+
+function Tabs(data){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = data;
+
+    return tab;
+}
+// console.log(Tabs('test'))
